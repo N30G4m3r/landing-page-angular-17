05140e5414e1c89dd9386c1657364f2164120843
N30G4m3r <neogamer1211@gmail.com>
2024-07-09 11:59:03 -0400

    Landing Page Angular 17

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { ProductsComponent } from './page/products/products.component';
+import { ProductsDetailComponent } from './page/products-detail/products-detail.component';
+import { ContactComponent } from './page/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'products/:id', component: ProductsDetailComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
